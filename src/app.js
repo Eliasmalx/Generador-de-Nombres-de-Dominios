ce9ad6f2d10f674899af648ec66023eb7f5908b9
Eliasmal Rodriguez <Eliasmal0228@gmail.com>
2025-03-23 20:28:38 +0000

    agregar combinacion en jss y estilos de la pagina

--- src/app.js
+++ src/app.js
@@ -6,10 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
   let noun = ['jogger', 'racoon'];
+  let domain = ['.com', '.net', '.es'];
 
-  
+  let lista = document.getElementById('lista-dominios');
+
+  let resultados = [];
+
+  pronoun.forEach((pronombre) => {
+    adj.forEach((adjetivo) => {
+      noun.forEach((sujeto) => {
+        domain.forEach((dominio) => {
+          resultados.push(`${pronombre}${adjetivo}${sujeto}${dominio}`);
+        });
+      });
+    });
+  });
+
+  lista.innerHTML = resultados.map(item => `<li>${item}</li>`).join('');
 };
